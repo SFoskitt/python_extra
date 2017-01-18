990e16bc7fb6854bd4c83a50c5c0a017207c4872
Stephanie Foskitt <stephanie.foskitt@gmail.com>
2017-01-18 15:36:09 -0800

    My100Days - add find_len.py

---
+++ find_len.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, candidate = argv
+
+def find_len(candidate):
+	i = 0
+	for item in candidate:
+		i += 1
+	return i
+
+print "length of candidate is", find_len(candidate)
